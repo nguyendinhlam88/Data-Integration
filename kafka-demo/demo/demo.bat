350ba3a058f55f4b391fc6331574d5f10f2cc179
Nguyen Huu Kiet <kietnguyen10112000@gmail.com>
2022-06-15 01:08:20 +0700

    Add kafka-demo

---
+++ kafka-demo/demo/demo.bat
@@ -0,0 +1,5 @@
+start cmd /C call python kafka_consumer.py
+:: start cmd /C call python kafka_consumer.py
+:: start cmd /C call python kafka_consumer.py
+:: start cmd /C call python kafka_consumer.py
+call python kafka_producer.py 5
