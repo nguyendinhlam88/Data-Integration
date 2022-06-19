e4bbee82308738884fae3d2944ba9f4b08d3c9c1
ghuioio <54397393+ghuioio@users.noreply.github.com>
2022-06-19 16:05:24 +0700

    carmudi

---
+++ WebScrapy/carmudi/carmudi/spiders/carmudi.py
@@ -0,0 +1,32 @@
+from pkg_resources import yield_lines
+import scrapy   
+from ..items import CarmudiItem
+class CarmudiSpider(scrapy.Spider):
+    name = "carmudi"
+    page_number = 1
+    base_url = "https://www.carmudi.vn/mua-ban-o-to-cu/"
+    start_urls = ["https://www.carmudi.vn/mua-ban-o-to-cu/"]
+
+    def parse(self, response):
+        all_ads = response.xpath('//a[@class="title is-5"]/@href').extract()
+        for ad_url in all_ads:
+            yield scrapy.Request(ad_url, callback=self.parse_ad)
+
+    def parse_ad(self, response):
+        item = CarmudiItem()
+        item['gia'] = response.xpath('//div[@class="price-tag"]/@data-price').get()
+        item['dong_xe'] = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "feature-item", " " )) and (((count(preceding-sibling::*) + 1) = 2) and parent::*)]//span/text()').extract()[0]
+        item['tinh_trang'] = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "feature-item", " " )) and (((count(preceding-sibling::*) + 1) = 5) and parent::*)]//text()').extract()[1].replace('\n','').replace('Tình trạng xe: ','')
+        item['nhien_lieu'] = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "feature-item", " " )) and (((count(preceding-sibling::*) + 1) = 9) and parent::*)]//text()').extract()[1].replace('\n','').replace('Nhiên liệu: ','')
+        item['hop_so'] = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "feature-item", " " )) and (((count(preceding-sibling::*) + 1) = 6) and parent::*)]//text()').extract()[1].replace('\n','').replace('Hộp số: ','')         
+        item['xuat_xu'] = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "feature-item", " " )) and (((count(preceding-sibling::*) + 1) = 12) and parent::*)]//text()').extract()[1].replace('\n','').replace('Xuất xứ: ','')
+        item['so_cho_ngoi'] = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "feature-item", " " )) and (((count(preceding-sibling::*) + 1) = 10) and parent::*)]//text()').extract()[1].replace('\n','').replace('Số ghế ngồi: ','')
+        item['mau_xe'] = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "feature-item", " " )) and (((count(preceding-sibling::*) + 1) = 11) and parent::*)]//text()').extract()[1].replace('\n','').replace('Màu ngoại thất: ','')
+        item['mo_ta'] = response.xpath('//*[(@id = "area_description")]//p/text()').extract()
+        next_page = 'https://www.carmudi.vn/mua-ban-o-to-cu/index{}.html'.format(CarmudiSpider.page_number)
+        if CarmudiSpider.page_number <= 50:
+            CarmudiSpider.page_number += 1
+            yield response.follow(next_page, callback=self.parse)
+        yield item
+
+    
