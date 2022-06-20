2729fb7088c561e7ffa8bbb4a3242e20af239ccf
lam.nguyen3 <lam.nguyen3@hebela.net>
2022-06-20 23:53:21 +0700

    code standard for project

--- WebScrapy/chotot_com/convert.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-# df = pd.read_json (r'D:\Code\Python\crawl_khdl\chotot_com\2511.json')
-# df.to_csv (r'D:\Code\Python\crawl_khdl\chotot_com\2511.csv', index = None)
-df = pd.read_csv (r'D:\Code\Python\crawl_khdl\chotot_com\a.csv')
-print(df)
